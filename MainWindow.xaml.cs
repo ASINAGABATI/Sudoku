4cb8e13e6ac27b3428e47fb983557b088ad62f73
sasaki <hisayuki.sasaki@technohighway.co.jp>
2024-01-12 14:07:08 +0900

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,392 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Sudoku
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Button? submit1;
+
+        private Button? clear;
+        private object colorAnswer;
+        private object colorQuestion;
+        private NumPlace numPlace;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            colorQuestion = rdoQuestion.Foreground;
+            colorAnswer= rdoAnswer.Foreground;
+
+            numPlace = new NumPlace();
+            initView();
+
+        }
+        private void initView(int index = -1)
+        {
+            Play play = numPlace.Load(index);
+            backImage.ImageSource = null;
+            foreach (string a in Constants.CELLNAME)
+            {
+                object obj = FindName(a);
+                Button btn = (Button)obj;
+                btn.Content = " ";
+            }
+            var items = new ObservableCollection<string>();
+            if (numPlace.plays.Count > 0)
+            {
+                foreach(var p in play.squares)
+                {
+                    //string s = String.Format("C{0}_{1}", p.col.ToString(), p.row.ToString());
+                    object obj = FindName(String.Format("C{0}_{1}", p.col.ToString(), p.row.ToString()) );
+                    Button btn = (Button)obj;
+                    btn.Content = p.num.ToString();
+                    btn.Foreground = p.cond == 0 ? (Brush)(colorQuestion) : (Brush)(colorAnswer);
+                }
+            }
+            if (index == -1)
+            {
+                var lst = numPlace.listGame();
+                lstHistory.ItemsSource = lst;
+                lstHistory.SelectedIndex = lst.Count - 1;
+            }
+        }
+
+        //private void CreateControls()
+        //{
+        //    firstNameLabel = new();
+        //    firstNameLabel.Content = "Enter your first name:";
+        //    grid1.Children.Add(firstNameLabel);
+
+        //    firstName = new TextBox();
+        //    firstName.Margin = new Thickness(0, 5, 10, 5);
+        //    Grid.SetColumn(firstName, 1);
+        //    grid1.Children.Add(firstName);
+
+        //    lastNameLabel = new Label();
+        //    lastNameLabel.Content = "Enter your last name:";
+        //    Grid.SetRow(lastNameLabel, 1);
+        //    grid1.Children.Add(lastNameLabel);
+
+        //    lastName = new TextBox();
+        //    lastName.Margin = new Thickness(0, 5, 10, 5);
+        //    Grid.SetColumn(lastName, 1);
+        //    Grid.SetRow(lastName, 1);
+        //    grid1.Children.Add(lastName);
+
+        //    submit = new Button();
+        //    submit.Content = "View message";
+        //    Grid.SetRow(submit, 2);
+        //    grid1.Children.Add(submit);
+
+        //    clear = new Button();
+        //    clear.Content = "Clear Name";
+        //    //clear.Style = "{StaticResource GradButton}";
+        //    Grid.SetRow(clear, 2);
+        //    Grid.SetColumn(clear, 1);
+        //    grid1.Children.Add(clear);
+        //}
+        //private void createButton()
+        //{
+        //    LinearGradientBrush buttonBrush = new LinearGradientBrush();
+        //    buttonBrush.StartPoint = new Point(0, 0.5);
+        //    buttonBrush.EndPoint = new Point(1, 0.5);
+        //    buttonBrush.GradientStops.Add(new GradientStop(Colors.Blue, 0));
+        //    buttonBrush.GradientStops.Add(new GradientStop(Colors.White, 0.9));
+
+        //    submit1 = new Button();
+        //    submit1.Content = "View message";
+        //    Grid.SetRow(submit1, 3);
+        //    grid1.Children.Add(submit1);
+
+        //    submit1.Background = buttonBrush;
+        //    submit1.FontSize = 14;
+        //    submit1.FontWeight = FontWeights.Bold;
+        //}
+
+        private void btnGetImage(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+
+                filePathToImageSource(openFileDialog.FileName);
+            }
+        }
+        private void btnGetClipboard(object sender, EventArgs e)
+        {
+            var obj = (System.IO.MemoryStream)Clipboard.GetData("PNG");
+            if (obj != null)
+            {
+                var bi = new BitmapImage();
+                bi.BeginInit();
+                bi.CacheOption = BitmapCacheOption.OnLoad;
+                bi.StreamSource = obj;
+                bi.EndInit();
+                bi.Freeze();
+                backImage.ImageSource = cnv(bi);
+            }
+        }
+        private void btnSaveAsGame(object sender, EventArgs e)
+        {
+            int idx0 = lstHistory.SelectedIndex;
+            int idx1;
+            if (idx0 == -1)
+            {
+                idx1 = lstHistory.Items.Count == 0 ? -1 : lstHistory.Items.Count - 1;
+            }
+            else
+            {
+                idx1 = idx0;
+            }
+            save(idx1);
+        }
+        private void btnSaveAddGame(object sender, EventArgs e)
+        {
+            save(-1);
+        }
+        private void save(int k)
+        {
+            Play play = new();
+            foreach (var cn in Constants.CELLNAME)
+            {
+                Button btn = (Button)FindName(cn);
+                Int32 n;
+                if (Int32.TryParse(btn.Content.ToString(), out n))
+                {
+                    string[] name = btn.Name.Substring(1).Split("_");
+                    var g = new Square();
+                    g.col = Int32.Parse(name[0]);
+                    g.row = Int32.Parse(name[1]);
+                    g.num = n;
+                    g.cond = btn.Foreground == (Brush)(colorQuestion) ? 0 : 1;
+                    play.squares.Add(g);
+                }
+                play.today = DateTime.Now.ToString();
+            }
+            if (play.squares.Count > 0)
+            {
+                if( k == -1)
+                {
+                    numPlace.plays.Add(play);
+                }
+                else
+                {
+                    numPlace.plays[k] = play;
+                }
+
+                numPlace.SaveSudoku2(numPlace);
+            }
+        }
+        private void btnDiagnose(object sender, EventArgs e)
+        {
+            int[,] board = new int[9, 9];
+
+            foreach (var cn in Constants.CELLNAME)
+            {
+                object obj = FindName(cn);
+                Button btn = (Button)obj;
+                int n;
+                if (Int32.TryParse(btn.Content.ToString(), out n))
+                {
+                    string[] name = cn.Substring(1).Split("_");
+                    int col = Int32.Parse(name[0]);
+                    int row = Int32.Parse(name[1]);
+                    board[col - 1, row - 1] = n;
+                    Debug.WriteLine(btn.Content.ToString() + " color:" + btn.Foreground.ToString() + " " + col + " " + row + " " + n);
+                }
+            }
+
+            int diag = 0;
+            for (int row = 1; row <= 9; row++)
+            {
+                Stack<int> hori = new Stack<int>();
+                for (int col = 1; col <= 9; col++)
+                {
+                    int n = board[col - 1, row - 1];
+                    if (n != 0)
+                    {
+                        if (hori.Contains(n))
+                        {
+                            diag = 1;   // 横に同一値
+                            break;
+                        }
+                        hori.Push(n);
+                    }
+                }
+                if (diag != 0) { break; }
+                if (hori.Count != 9)
+                {
+                    diag = 2;   // 空がある
+                    break;
+                }
+            }
+            if (diag != 0)
+            {
+                MessageBox.Show("Diagnose " + diag);
+                return;
+            }
+            for (int col = 1; col <= 9; col++)
+            {
+                Stack<int> vert = new Stack<int>();
+                for (int row = 1; row <= 9; row++)
+                {
+                    int n = board[col - 1, row - 1];
+                    if (vert.Contains(n))
+                    {
+                        diag = 11;   // 縦に同一値
+                        break;
+                    }
+                    vert.Push(n);
+                }
+                if (diag != 0) { break; }
+            }
+            if (diag != 0)
+            {
+                MessageBox.Show("Diagnose " + diag);
+                return;
+            }
+        }
+        private void filePathToImageSource(string filePath)
+        {
+            try
+            {
+                var bi = new BitmapImage();
+                using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    bi.BeginInit();
+
+                    bi.CacheOption = BitmapCacheOption.OnLoad;
+                    bi.StreamSource = fs;
+                    bi.EndInit();
+                }
+                bi.Freeze();
+
+                backImage.ImageSource = cnv(bi);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private BitmapSource cnv(BitmapImage bitmapimageOriginal)
+        {
+            // BitmapImageのPixelFormatをPbgra32に変換する
+            FormatConvertedBitmap bitmap = new FormatConvertedBitmap(bitmapimageOriginal, PixelFormats.Pbgra32, null, 0);
+
+            // 画像の大きさに従った配列を作る
+            int width = bitmap.PixelWidth;
+            int height = bitmap.PixelHeight;
+            byte[] originalPixcels = new byte[width * height * 4];
+            byte[] inversedPixcels = new byte[width * height * 4];
+
+            // BitmapSourceから配列にコピー
+            int stride = (width * bitmap.Format.BitsPerPixel + 7) / 8;
+            bitmap.CopyPixels(originalPixcels, stride, 0);
+
+            // 色を反転する
+            for (int x = 0; x < originalPixcels.Length; x = x + 4)
+            {
+                inversedPixcels[x] = (byte)(255 - originalPixcels[x]);
+                inversedPixcels[x + 1] = 255; // (byte)(255 - originalPixcels[x + 1]);
+                inversedPixcels[x + 2] = (byte)(originalPixcels[x + 2]);
+                inversedPixcels[x + 3] = originalPixcels[x + 3];
+            }
+            // 配列からBitmaopSourceを作る
+            BitmapSource inversedBitmap = BitmapSource.Create(width, height, 96, 96, PixelFormats.Pbgra32, null, inversedPixcels, stride);
+
+            return inversedBitmap;
+        }
+
+        private void btnEraseImage(object sender, EventArgs e)
+        {
+            backImage.ImageSource = null;
+        }
+        private void btnResetItem(object sender, EventArgs e)
+        {
+            foreach (string a in Constants.CELLNAME)
+            {
+                object obj = FindName(a);
+                Button btn = (Button)obj;
+                if (btn.Foreground == (Brush)colorAnswer)
+                {
+                    btn.Content = " ";
+                }
+            }
+        }
+        private void btnEraseItem(object sender, EventArgs e)
+        {
+            foreach(string a in Constants.CELLNAME)
+            {
+                object obj = FindName(a);
+                Button btn = (Button)obj;
+                btn.Content = " ";
+            }
+        }
+        private void btnNumberItem(object sender, EventArgs e)
+        {
+            Button lbl = (Button)sender;
+
+            I0.Content = lbl.Content.ToString();
+        }
+
+        private void selectCellPreRBDown(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = (Button)FindName(((Button)sender).Name);
+            btn.Content = "";
+        }
+        private void selectCellDown(object sender, EventArgs e)
+        {
+            Button btn = (Button)FindName(((Button)sender).Name);
+            btn.Content = I0.Content;
+            btn.Foreground = ((bool)(rdoQuestion.IsChecked)) ? (Brush)(colorQuestion) : (Brush)(colorAnswer);
+        }
+        private void selectCellWheel(object sender, MouseWheelEventArgs e)
+        {
+            Button btn = (Button)FindName(((Button)sender).Name);
+            int n;
+            if (!Int32.TryParse(btn.Content.ToString(), out n))
+            {
+                n = 0;
+            }
+            n += e.Delta / Math.Abs(e.Delta);
+            if (n < 0)
+            {
+                n = 9;
+            }
+            else if (n > 9)
+            {
+                n = 0;
+            }
+            btn.Content = n == 0 ? " " : n.ToString();
+            btn.Foreground = ((bool)(rdoQuestion.IsChecked)) ? (Brush)(colorQuestion) : (Brush)(colorAnswer);
+        }
+        private void lstBoxSelect(object sender, EventArgs e)
+        {
+            int idx = (sender as ListBox).SelectedIndex;
+            initView(idx);
+        }
+        private void deleteGame(object sender, EventArgs e)
+        {
+            int idx = lstHistory.SelectedIndex;
+            sblbl1.Content = idx.ToString();
+            numPlace.plays.RemoveAt(idx);
+            numPlace.SaveSudoku2(numPlace);
+            initView();
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Security.Policy;
+
+namespace Sudoku
+{
+    static class Constants
+    {
+        public const string CONF_FILENAME = "sudoku.json";
+
+        public static readonly string[,] CELLNAME = {
+                            { "C9_1", "C8_1", "C7_1", "C6_1", "C5_1", "C4_1", "C3_1", "C2_1", "C1_1" },
+                            { "C9_2", "C8_2", "C7_2", "C6_2", "C5_2", "C4_2", "C3_2", "C2_2", "C1_2" },
+                            { "C9_3", "C8_3", "C7_3", "C6_3", "C5_3", "C4_3", "C3_3", "C2_3", "C1_3" },
+
+                            { "C9_4", "C8_4", "C7_4", "C6_4", "C5_4", "C4_4", "C3_4", "C2_4", "C1_4" },
+                            { "C9_5", "C8_5", "C7_5", "C6_5", "C5_5", "C4_5", "C3_5", "C2_5", "C1_5" },
+                            { "C9_6", "C8_6", "C7_6", "C6_6", "C5_6", "C4_6", "C3_6", "C2_6", "C1_6" },
+
+                            { "C9_7", "C8_7", "C7_7", "C6_7", "C5_7", "C4_7", "C3_7", "C2_7", "C1_7" },
+                            { "C9_8", "C8_8", "C7_8", "C6_8", "C5_8", "C4_8", "C3_8", "C2_8", "C1_8" },
+                            { "C9_9", "C8_9", "C7_9", "C6_9", "C5_9", "C4_9", "C3_9", "C2_9", "C1_9" }
+            };
+    }
+
+    internal class OnePeace
+    {
+        public int col { get; set; }    // 左から 1,2,3,4,5,6,7,8,9
+        public int row { get; set; }    // 上から 1,2,3,4,5,6,7,8,9
+        public int num { get; set; }    // 0:KURO(Question), 1:AKA(Answer)
+
+        public OnePeace()
+        {
+            col = 0;
+            row = 0;
+            num = 0;
+        }
+    }
+    internal class OneNumPeace
+    {
+        public string today { get; set; }
+        public List<OnePeace> onePeace { get; set; }
+
+        public OneNumPeace()
+        {
+            today = DateTime.Now.ToString();
+            onePeace = new List<OnePeace> { };
+        }
+    }
+
+    public class Square
+    {
+        public int col { get; set; }    // 左から 1,2,3,4,5,6,7,8,9
+        public int row { get; set; }    // 上から 1,2,3,4,5,6,7,8,9
+        public int num { get; set; }    // 1,2,3,4,5,6,7,8,9
+        public int cond { get; set; }   // 0:KURO(Question), 1:AKA(Answer)
+
+        public Square()
+        {
+            col = 0;
+            row = 0;
+            num = 0;
+            cond = 0;
+        }
+        public Square(int a, int b, int c, int d)
+        {
+            col = a;
+            row = b;
+            num = c;
+            cond = d;
+        }
+    }
+    public class Play
+    {
+        public string? today { get; set; }
+        public List<Square> squares { get; set; }
+
+        public Play()
+        {
+            today = "";
+            squares = new List<Square>();
+        }
+    }
+    public class NumPlace
+    {
+        public int number { get; set; }
+        public List<Play> plays { get; set; }
+        private string confPath = System.IO.Path.Combine(Environment.CurrentDirectory, Constants.CONF_FILENAME);
+
+        public NumPlace()
+        {
+            number = 0;
+            plays = new List<Play>();
+        }
+
+        public Play Load(int index = -1)
+        {
+            Play r = new Play();
+            try
+            {
+                string jsonString = File.ReadAllText(confPath);
+                if (jsonString != null)
+                {
+                    var rdata = JsonSerializer.Deserialize<NumPlace>(jsonString);
+                    if (rdata != null)
+                    {
+                        number = rdata.number;
+                        plays = rdata.plays;
+
+                        if (index < 0)
+                        {
+                            r = plays[plays.Count - 1];
+                        }
+                        else
+                        {
+                            if (index < plays.Count)
+                            {
+                                r = plays[index];
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show(ex.Message);
+            }
+            return r;
+        }
+        public bool SaveSudoku2(NumPlace np)
+        {
+            bool r = true;
+            try
+            {
+                var json_str = JsonSerializer.Serialize(np, new JsonSerializerOptions { WriteIndented = true });
+
+                string confPath = System.IO.Path.Combine(Environment.CurrentDirectory, Constants.CONF_FILENAME);
+                File.WriteAllText(confPath, json_str);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                r = false;
+            }
+            return r;
+        }
+        public List<string> listGame()
+        {
+            List<string> lst = new();
+            foreach(var p in plays)
+            {
+                lst.Add(p.today);
+            }
+            return lst;
+        }
+    }
+
+}
